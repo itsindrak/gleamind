629b933cfd9887fba0cd02170461990f29dccadd
itsindrak <itsindrak@gmail.com>
2024-07-11 13:30:44 +0530

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,3 @@
-document.getElementById('alertButton').addEventListener('click', function() {
-    alert('Button clicked!');
-});
+function scrollToSection(sectionId) {
+    document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
+}
